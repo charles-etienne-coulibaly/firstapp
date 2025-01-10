5f220e0d7446ff1f1ed5bef218cc502cb0435f0f
charles-etienne-coulibaly <coulibalycharles23@gmail.com>
2025-01-10 17:03:33 +0000

    Mise en place de la page accueil avec une barre de navigation

---
+++ lib/accueil.dart
@@ -0,0 +1,59 @@
+import 'package:firstapp/models/planning.dart';
+import 'package:firstapp/widgets/planning_widget.dart';
+import 'package:flutter/material.dart';
+
+class Accueil extends StatefulWidget {
+  const Accueil({super.key});
+
+  @override
+  State<Accueil> createState() => _AccueilState();
+}
+
+class _AccueilState extends State<Accueil> {
+  List<Planning> datas = [
+    Planning("Anglais", "maison", "description english", "12/12/2024",
+        "images/apple.png"),
+    Planning("Anglais1", "maison1", "description english1", "19/12/2024",
+        "images/facebook.png"),
+    Planning(
+        "Anglais2",
+        "maison2",
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        "26/12/2024",
+        "images/fond.jpg"),
+    Planning(
+        "Anglais3",
+        "maison3",
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.3",
+        "03/01/2025",
+        "images/google.png"),
+    Planning(
+        "Element1",
+        "maison2",
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.2",
+        "26/12/2024",
+        "images/fond.png"),
+    Planning(
+        "Element2",
+        "maison3",
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.3",
+        "03/01/2025",
+        "images/proxy-image.png"),
+  ];
+  int selectedItem = 0;
+  List<Widget> pages = [
+    Accueil(),
+    Center(child: Text("Planning")),
+    Center(child: Text("A propos")),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: ListView.builder(
+            itemCount: datas.length,
+            itemBuilder: (context, int index) {
+              return PlanningWidget(planning: datas[index]);
+            }));
+  }
+}
--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,5 @@
+import 'package:firstapp/accueil.dart';
 import 'package:firstapp/login.dart';
-import 'package:firstapp/models/planning.dart';
-import 'package:firstapp/widgets/planning_widget.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -11,36 +10,19 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  List<Planning> datas = [
-    Planning("Anglais", "maison", "description english", "12/12/2024",
-        "images/apple.png"),
-    Planning("Anglais1", "maison1", "description english1", "19/12/2024",
-        "images/facebook.png"),
-    Planning(
-        "Anglais2",
-        "maison2",
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-        "26/12/2024",
-        "images/fond.jpg"),
-    Planning(
-        "Anglais3",
-        "maison3",
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.3",
-        "03/01/2025",
-        "images/google.png"),
-    Planning(
-        "Element1",
-        "maison2",
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.2",
-        "26/12/2024",
-        "images/fond.png"),
-    Planning(
-        "Element2",
-        "maison3",
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.3",
-        "03/01/2025",
-        "images/proxy-image.png"),
+  int selectedItem = 0;
+  List<Widget> pages = [
+    Accueil(),
+    Center(child: Text("Planning")),
+    Center(child: Text("A propos")),
   ];
+  void cliqueNavigation(int index) {
+    setState(() {
+      selectedItem = index;
+      print("index =$selectedItem");
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -51,66 +33,79 @@ class _HomeState extends State<Home> {
               width: 150,
               height: 150,
               decoration: BoxDecoration(
-                image: DecorationImage(image: AssetImage("images/facebook.png")),
+                image:
+                    DecorationImage(image: AssetImage("images/facebook.png")),
               ),
             ),
             Center(
-                child: Text("user@gmail.com"),),
-            InkWell(
-              onTap: (){
-                Navigator.pushReplacement(context,
-                    MaterialPageRoute(builder: (context) {
-                      return const Home();
-                    }));
-              },
-              child: ListTile(title: Text("Home"),
-              leading: Icon(Icons.home),),
-            ),
-            Divider(
+              child: Text("user@gmail.com"),
             ),
             InkWell(
-              onTap: (){
+              onTap: () {
                 Navigator.pushReplacement(context,
                     MaterialPageRoute(builder: (context) {
-                      return const Home();
-                    }));
+                  return const Home();
+                }));
               },
-              child: ListTile(title: Text("Planning exécute"),
-                leading: Icon(Icons.list),),
+              child: ListTile(
+                title: Text("Home"),
+                leading: Icon(Icons.home),
+              ),
             ),
-            Divider(
-            ),
+            Divider(),
             InkWell(
-              onTap: (){
+              onTap: () {
                 Navigator.pushReplacement(context,
                     MaterialPageRoute(builder: (context) {
-                      return const Home();
-                    }));
+                  return const Home();
+                }));
               },
-              child: ListTile(title: Text("A propos"),
-                leading: Icon(Icons.person),),
+              child: ListTile(
+                title: Text("Planning exécute"),
+                leading: Icon(Icons.list),
+              ),
             ),
-            Divider(
-            ),
+            Divider(),
             InkWell(
-              onTap: (){
+              onTap: () {
                 Navigator.pushReplacement(context,
                     MaterialPageRoute(builder: (context) {
-                      return const Login();
-                    }));
+                  return const Home();
+                }));
               },
-              child: ListTile(title: Text("Déconnexion"),
-                leading: Icon(Icons.logout),),
+              child: ListTile(
+                title: Text("A propos"),
+                leading: Icon(Icons.person),
+              ),
             ),
-            Divider(
+            Divider(),
+            InkWell(
+              onTap: () {
+                Navigator.pushReplacement(context,
+                    MaterialPageRoute(builder: (context) {
+                  return const Login();
+                }));
+              },
+              child: ListTile(
+                title: Text("Déconnexion"),
+                leading: Icon(Icons.logout),
+              ),
             ),
-
+            Divider(),
           ],
         ),
       ),
-        appBar: AppBar(
-
-          /*last logout button
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: selectedItem,
+          onTap: cliqueNavigation,
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(icon: Icon(Icons.list), label: "Planning"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.person), label: "A propos"),
+          ]),
+      appBar: AppBar(
+        /*last logout button
           actions: [
             IconButton(
                 onPressed: () {
@@ -121,14 +116,11 @@ class _HomeState extends State<Home> {
                 },
                 icon: const Icon(Icons.logout))
           ],*/
-          title: const Text("Page d'Accueil"),
-          elevation: 10,
-          backgroundColor: Colors.cyan,
-        ),
-        body: ListView.builder(
-            itemCount: datas.length,
-            itemBuilder: (context, int index) {
-              return PlanningWidget(planning: datas[index]);
-            }));
+        title: const Text("Page d'Accueil"),
+        elevation: 10,
+        backgroundColor: Colors.cyan,
+      ),
+      body: pages[selectedItem],
+    );
   }
 }
